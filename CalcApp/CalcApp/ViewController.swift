0c979e5f68532e3b8314a3ee775ddbf0f37bdecc
seitovnurlan <seitov@gmail.com>
2021-02-06 22:13:36 +0600

    project initialization

---
+++ CalcApp/CalcApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by Nurlan Seitov on 6/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
