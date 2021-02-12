daaccd08c0c84ef67f9545935b8ac7080c255099
seitovnurlan <seitov@gmail.com>
2021-02-12 23:22:39 +0600

    working with code (Part 1)

--- CalcApp/CalcApp/ViewController.swift
+++ CalcApp/CalcApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var displayResultLabel: UILabel!
+    var stilltyping = false
+    
+    @IBAction func namberPressed(_ sender: UIButton) {
+        
+        let number = sender.currentTitle!
+        
+        if stilltyping {
+            if let text = displayResultLabel.text,text.count < 20 {
+        displayResultLabel.text = displayResultLabel.text! + number
+            }
+        } else {
+            displayResultLabel.text = number
+            stilltyping = true
+        }
     }
-
+    
 
 }
 
